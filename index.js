5a65f9abfdaa177062c4f50fce2cd944f2418541
ascollege82-prog <ascollege82@gmail.com>
2026-05-05 18:47:34 +0530

    Fix admin seeding validation error

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const UserSchema = new mongoose.Schema({
     type: String, required: [true, 'Please add an email'], unique: true,
     match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
   },
-  password: { type: String, required: [true, 'Please add a password'], minlength: 6, select: false },
+  password: { type: String, required: [true, 'Please add a password'], minlength: 3, select: false },
   role: { type: String, enum: ['student', 'recruiter', 'admin'], default: 'student' },
   skills: { type: [String], default: [] },
   resume: { type: String, default: '' },
